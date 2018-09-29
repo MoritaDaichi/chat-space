cec89f2d93394dd4cfded3ef641910526a3e909f
MoritaDaichi <kireazi19990424@gmail.com>
2018-09-29 20:32:37 +0900

    some

--- app/assets/javascripts/asynchronous.js
+++ app/assets/javascripts/asynchronous.js
@@ -6,34 +6,30 @@ $(function() {
   }
   var html = `
     <div class="upper-message" data-message-id="${message.id}">
-      <div class="upper-message__user-name">${message.name}</div>
-      <div class="upper-message__date">${message.date}</div>
-      <div class="lower-message__content">${message.content}</div>
+      <div class="upper-message__user-name">
+        ${message.name}
+      </div>
+      <div class="upper-message__date">
+        ${message.date}
+      </div>
+      <div class="lower-message__content">
+        ${message.content}
+      </div>
       ${insertImage}
     </div>`;
   return html
 }
 
- var interval = setInterval(function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)) {
-  $.ajax({
-    url: location.href.json,
-  })
-  .done(function(data) {
-    var insertHTML = '';
-    console.log(data.messages);
-    if(data.messages != null ){
-      data.messages.forEach(function(i,message) {
-        insertHTML += buildHTML(message);
-      });
-      $('.messages').html(insertHTML);
-    }
-  })
-  .fail(function(json) {
-    alert('自動更新に失敗しました');
-  });
-  } else {
-    clearInterval(interval);
-  }} , 5 * 1000 );
+ setInterval(function(){
+    $.ajax({
+      url: location.href.json,
+    })
+    .done(function(data) {
+      console.log(data.messages);
+    })
+    .fail(function(json) {
+      alert('自動更新に失敗しました');
+    });
+    } ,5000 );
 });
 
